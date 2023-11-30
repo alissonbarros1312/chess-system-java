0fe577150ae610a91a8f46862808cac9312c9571
Alisson <barrosalisson1312@gmail.com>
2023-11-29 21:28:39 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
-		UI.printBoard(match.getPiece());
+		
+		while(true) {
+			UI.printBoard(match.getPiece());
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = match.perfomChessMove(source, target);
+		}
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -26,6 +30,19 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+
+		try {
+			String pos = sc.nextLine();
+			char column = pos.charAt(0);
+			Integer row = Integer.parseInt(pos.substring(1)); // recorta a string e converte para inteiro
+
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition: Valid values are from a1 to h8");
+		}
+	}
+
 	public static void printBoard(ChessPiece[][] piece) { // metodo que ira pegar a matriz passada no parametro,
 															// e imprimir o tabuleiro com as peças
 
@@ -43,7 +60,7 @@ public class UI {
 		if (piece == null) {
 			System.out.print("-");
 		} else {
-			if(piece.getColor() == Color.WHITE) {
+			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
 			} else {
 				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
